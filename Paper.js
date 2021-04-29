2bf986c2afa49e679f06a387be647fa79675f5d9
Saniaphatak <78636257+Saniaphatak@users.noreply.github.com>
2021-04-29 19:04:15 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,35 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2,
+        			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.body= Bodies.circle(x, y, r, options);
+        this.image = loadImage("paper.png");
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var Pos=this.body.position;		
+
+			push()
+			translate(Pos.x, Pos.y);
+			imageMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.image,0,0,this.width, this.height);
+			pop()
+			
+	}
+
+}
